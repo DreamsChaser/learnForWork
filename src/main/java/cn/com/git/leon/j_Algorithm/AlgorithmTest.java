40b2fc46ecf66d13ac1ebbce9d873e3718a3431e
wangdi <18661230709@163.com>
2019-05-06 00:05:20 +0800

    '20190506

---
+++ src/main/java/cn/com/git/leon/j_Algorithm/AlgorithmTest.java
@@ -0,0 +1,8 @@
+package cn.com.git.leon.j_Algorithm;
+
+/**
+ * @author sirius
+ * @since 2019/5/4
+ */
+public class AlgorithmTest {
+}
---
+++ src/main/java/cn/com/git/leon/j_Algorithm/Bitmap/UserTagCount.java
@@ -0,0 +1,12 @@
+package cn.com.git.leon.j_Algorithm.Bitmap;
+
+/**
+ * 用户标签统计
+ * 每个标签作为一个bitmap 可以通过&和|操作求并集交集
+ * @author sirius
+ * @since 2019/5/4
+ */
+public class UserTagCount {
+    public static void main(String[] args) {
+    }
+}

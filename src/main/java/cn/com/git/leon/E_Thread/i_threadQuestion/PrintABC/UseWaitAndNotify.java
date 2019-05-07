658aa718b0c16b4a78e7a5439ab0723396d83ffa
wangdi <18661230709@163.com>
2019-05-07 17:38:36 +0800

    '20190507'

---
+++ src/main/java/cn/com/git/leon/E_Thread/i_threadQuestion/PrintABC/UseWaitAndNotify.java
@@ -0,0 +1,12 @@
+package cn.com.git.leon.E_Thread.i_threadQuestion.PrintABC;
+
+/**
+ * @author sirius
+ * @since 2019/5/7
+ */
+public class UseWaitAndNotify {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/cn/com/git/leon/E_Thread/i_threadQuestion/ThreadQuestionTest.java
@@ -0,0 +1,8 @@
+package cn.com.git.leon.E_Thread.i_threadQuestion;
+
+/**
+ * @author sirius
+ * @since 2019/5/7
+ */
+public class ThreadQuestionTest {
+}

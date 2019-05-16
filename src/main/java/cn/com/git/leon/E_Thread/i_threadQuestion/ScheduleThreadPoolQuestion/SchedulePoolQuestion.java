31b61e87b5311343ac215a68261a6981efe445f3
wangdi <18661230709@163.com>
2019-05-17 00:20:19 +0800

    '20190516'

--- src/main/java/cn/com/git/leon/E_Thread/i_threadQuestion/ScheduleThreadPoolQuestion/SchedulePoolQuestion.java
+++ src/main/java/cn/com/git/leon/E_Thread/i_threadQuestion/ScheduleThreadPoolQuestion/SchedulePoolQuestion.java
@@ -1,8 +1,6 @@
 package cn.com.git.leon.E_Thread.i_threadQuestion.ScheduleThreadPoolQuestion;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * 假如有一个第三方接口，有很多个线程去调用获取数据，
@@ -11,8 +9,15 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
  * @since 2019/5/16
  */
 public class SchedulePoolQuestion {
-
     public static void main(String[] args) {
-
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("我执行了");
+            }
+        };
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(10);
+        executor.scheduleAtFixedRate(runnable,5L,2L,TimeUnit.SECONDS);
     }
+
 }

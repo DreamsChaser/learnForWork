8997e2d2e89306ff35a86b518adc6b701d239d33
wangdi <18661230709@163.com>
2019-05-16 17:44:26 +0800

    '20190516'

---
+++ src/main/java/cn/com/git/leon/E_Thread/c_AQS/AbstractQueuedSynchronizerDemo.java
@@ -0,0 +1,13 @@
+package cn.com.git.leon.E_Thread.c_AQS;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author sirius
+ * @since 2019/5/16
+ */
+public class AbstractQueuedSynchronizerDemo {
+    public static void main(String[] args) {
+        AbstractQueuedSynchronizer aqs = new AbstractQueuedSynchronizer() {};
+    }
+}
---
+++ src/main/java/cn/com/git/leon/E_Thread/i_threadQuestion/ScheduleThreadPoolQuestion/SchedulePoolQuestion.java
@@ -0,0 +1,18 @@
+package cn.com.git.leon.E_Thread.i_threadQuestion.ScheduleThreadPoolQuestion;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
+ * 假如有一个第三方接口，有很多个线程去调用获取数据，
+ * 现在规定每秒钟最多有 10 个线程同时调用它，如何做到。
+ * @author sirius
+ * @since 2019/5/16
+ */
+public class SchedulePoolQuestion {
+
+    public static void main(String[] args) {
+
+    }
+}

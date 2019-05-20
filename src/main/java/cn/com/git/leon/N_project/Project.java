d28af711550faade4b3100cb0d28c9a70819e34f
wangdi <18661230709@163.com>
2019-05-20 15:34:21 +0800

    '20190520'

--- src/main/java/cn/com/git/leon/N_project/Project.java
+++ src/main/java/cn/com/git/leon/N_project/Project.java
@@ -1,17 +1,63 @@
 package cn.com.git.leon.N_project;
 
+import java.io.File;
+import java.util.stream.Collectors;
+
 /**
  * @author sirius
  * @since 2019/5/14
  */
 public class Project {
+
+    private static int count = 0;
+
     public static void main(String[] args) {
-        //第二种情况
-        String temp = "hh";
-        String s1 = "a" + temp;
-        String s2 = "ahh";
-        String s3 = "hh";
-        System.out.println(s1 == s2);
-        System.out.println(temp == s3);
+        String str = "D:\\workspace\\learnForWork\\src\\main\\java\\cn\\com\\git\\leon";
+        File file = new File(str);
+//        test(file);
+        countFiles(file);
+        System.out.println(count);
+    }
+
+    public static void test(File in){
+        if (in.isDirectory()){
+            File[] files = in.listFiles();
+            if (files == null || files.length == 0){
+                return;
+            }
+            for (File file : files){
+                if (file.isDirectory()){
+                    test(file);
+                } else {
+                    String fileName = file.getName();
+                    String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
+                    if ("java".equals(suffix)){
+                        String absFileName = file.getAbsolutePath();
+                        file.renameTo(new File(absFileName.replace(".java",".txt")));
+                    }else if ("txt".equals(suffix)){
+                        continue;
+                    }else if ("png".equals(suffix)){
+                        continue;
+                    }else {
+                        String absFileName = file.getAbsolutePath();
+                        file.renameTo(new File(absFileName.concat(".txt")));
+                    }
+                }
+            }
+        }
+    }
+
+    public static void countFiles(File in){
+        if (in.isDirectory()){
+            File[] files = in.listFiles();
+            if (files == null || files.length == 0){
+                return;
+            }
+            for (File file : files){
+                countFiles(file);
+            }
+        } else {
+            count++;
+        }
     }
 }

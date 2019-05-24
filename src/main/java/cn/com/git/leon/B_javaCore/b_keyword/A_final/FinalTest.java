6b56b31092fc0919433bdbb78ad1f96fb5b03ba6
wangdi <18661230709@163.com>
2019-05-24 17:43:04 +0800

    '20190524'

---
+++ src/main/java/cn/com/git/leon/B_javaCore/b_keyword/A_final/FinalTest.java
@@ -0,0 +1,16 @@
+package cn.com.git.leon.B_javaCore.b_keyword.A_final;
+
+/**
+ * @author sirius
+ * @since 2019/5/24
+ */
+public class FinalTest {
+
+    private final String STR = "final";
+
+    private String str;
+
+    public void test(){
+        System.out.println(STR+str);
+    }
+}

0c69b9d442ca9e175f2f84b363454912bf6814bd
wangdi <18661230709@163.com>
2019-05-27 15:50:05 +0800

    '20190527'

--- src/main/java/cn/com/git/leon/E_Thread/e_blockQueue/B_LinkedBlockingQueue/LinkedBlockingQueueDemo.java
+++ src/main/java/cn/com/git/leon/E_Thread/e_blockQueue/B_LinkedBlockingQueue/LinkedBlockingQueueDemo.java
@@ -1,6 +1,7 @@
 package cn.com.git.leon.E_Thread.e_blockQueue.B_LinkedBlockingQueue;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author sirius
@@ -9,16 +10,32 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class LinkedBlockingQueueDemo {
 
     public static void main(String[] args) {
-        LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
-        try {
-            linkedBlockingQueue.put("a");
-            linkedBlockingQueue.put("b");
-            linkedBlockingQueue.put("c");
-            linkedBlockingQueue.take();
-            linkedBlockingQueue.take();
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        LinkedBlockingQueue<Integer> linkedBlockingQueue = new LinkedBlockingQueue<Integer>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0;i<10;i++)
+                    try {
+                        linkedBlockingQueue.put(i);
+                        System.out.println("放入"+i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (!linkedBlockingQueue.isEmpty()) {
+                        TimeUnit.SECONDS.sleep(2);
+                        int i = linkedBlockingQueue.take();
+                        System.out.println(i);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- src/main/java/cn/com/git/leon/E_Thread/e_blockQueue/E_SynchronousQueue/SynchronousQueueDemo.java
+++ src/main/java/cn/com/git/leon/E_Thread/e_blockQueue/E_SynchronousQueue/SynchronousQueueDemo.java
@@ -1,18 +1,53 @@
 package cn.com.git.leon.E_Thread.e_blockQueue.E_SynchronousQueue;
 
 import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author sirius
  * @since 2019/4/25
  */
 public class SynchronousQueueDemo {
+
+    static int count = 0;
+
     public static void main(String[] args) {
-        SynchronousQueue synchronousQueue = new SynchronousQueue();
-        try {
-            synchronousQueue.put("a");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        SynchronousQueue<Integer> synchronousQueue = new SynchronousQueue<Integer>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        int i = (int) (Math.random() * 10);
+                        System.out.println("线程睡" + i);
+                        TimeUnit.SECONDS.sleep(i);
+                        synchronousQueue.put(i);
+                        count++;
+                        System.out.println("第"+count+"次放入"+i);
+                        if (count == 10){
+                            break;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        int i = synchronousQueue.take();
+                        System.out.println("第"+count+"次取出"+i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (count == 10){
+                        break;
+                    }
+                }
+            }
+        }).start();
     }
 }

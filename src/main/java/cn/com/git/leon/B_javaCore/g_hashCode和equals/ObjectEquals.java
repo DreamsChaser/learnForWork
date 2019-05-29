ca44562acb63c15f5dd6b45604469f5f8f072f3d
wangdi <18661230709@163.com>
2019-05-30 01:22:47 +0800

    '20190530'

---
+++ src/main/java/cn/com/git/leon/B_javaCore/g_hashCode和equals/ObjectEquals.java
@@ -0,0 +1,13 @@
+package cn.com.git.leon.B_javaCore.g_hashCode和equals;
+
+/**
+ * @author sirius
+ * @since 2019/5/26
+ */
+public class ObjectEquals {
+    public static void main(String[] args) {
+        Object a = new Object();
+        Object b = new Object();
+        System.out.println(a.equals(b));
+    }
+}
---
+++ src/main/java/cn/com/git/leon/D_collection/g_hashTable/HashTableDemo.java
@@ -0,0 +1,13 @@
+package cn.com.git.leon.D_collection.g_hashTable;
+
+import java.util.Hashtable;
+
+/**
+ * @author sirius
+ * @since 2019/5/26
+ */
+public class HashTableDemo {
+    public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+    }
+}
